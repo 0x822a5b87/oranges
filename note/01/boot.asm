8836db3094d946d2d29329cf45e1e829860552a2
hangyudu <hangyudu@tencent.com>
2020-08-13 13:24:51 +0800

    add chapter 1

---
+++ note/01/boot.asm
@@ -0,0 +1,19 @@
+	org	0x7c00			; 告诉编译器程序加载到7c00处
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
+	call	DispStr			; 调用显示字符串例程
+    jmp $$
+DispStr:
+	mov	ax, BootMessage
+	mov	bp, ax			; ES:BP = 串地址
+	mov	cx, 16			; CX = 串长度
+	mov	ax, 0x1301		; AH = 13,  AL = 01h
+	mov	bx, 0x00c		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
+	mov	dl, 0
+	int	0x10			; 10h 号中断
+	ret
+
+BootMessage:		db	"Hello, OS world!"
+times 	510-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
+dw 	0xaa55				; 结束标志
